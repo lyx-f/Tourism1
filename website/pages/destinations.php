42add71675850f4a258c56f87698b8d0761f3c80
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2025-04-10 15:04:47 +0800

    updated destination layout

--- website/pages/destinations.php
+++ website/pages/destinations.php
@@ -26,50 +26,50 @@ if ($result->num_rows > 0) {
 </head>
 
 <body>
-<div class="grid-container">
-    <a href="events.php" class="card">
-        <img src="../../assets/img/events" alt="Events">
-        <div class="card-content">
-            <i class="far fa-calendar"></i>
-            <p>EVENTS</p>
-        </div>
-    </a>
+    <div class="grid-container">
+        <a href="events.php" class="card">
+            <img src="../../assets/img/events" alt="Events">
+            <div class="card-content">
+                <i class="far fa-calendar"></i>
+                <p>EVENTS</p>
+            </div>
+        </a>
 
-    <a href="attractions.php" class="card">
-        <img src="../../assets/img/attractions" alt="Attractions">
-        <div class="card-content">
-            <i class="fas fa-camera"></i>
-            <p>ATTRACTIONS</p>
-        </div>
-    </a>
+        <a href="attractions.php" class="card">
+            <img src="../../assets/img/attractions" alt="Attractions">
+            <div class="card-content">
+                <i class="fas fa-camera"></i>
+                <p>ATTRACTIONS</p>
+            </div>
+        </a>
 
-    <a href="restaurants.php" class="card">
-        <img src="../../assets/img/restaurants" alt="Restaurants">
-        <div class="card-content">
-            <i class="fas fa-utensils"></i>
-            <p>RESTAURANTS</p>
-        </div>
-    </a>
+        <a href="restaurants.php" class="card">
+            <img src="../../assets/img/restaurants" alt="Restaurants">
+            <div class="card-content">
+                <i class="fas fa-utensils"></i>
+                <p>RESTAURANTS</p>
+            </div>
+        </a>
 
-    <a href="accommodations.php" class="card">
-        <img src="../../assets/img/accommodation.webp" alt="Accommodations">
-        <div class="card-content">
-            <i class="fas fa-home"></i>
-            <p>ACCOMMODATIONS</p>
-        </div>
-    </a>
+        <a href="accommodations.php" class="card">
+            <img src="../../assets/img/accommodation.webp" alt="Accommodations">
+            <div class="card-content">
+                <i class="fas fa-home"></i>
+                <p>ACCOMMODATIONS</p>
+            </div>
+        </a>
 
-    <a href="see-more.php" class="card">
-        <img src="../../assets/img/lovemati" alt="See More">
-        <div class="card-content">
-            <i class="fas fa-search-location"></i>
-            <p>SEE MORE</p>
-        </div>
-    </a>
-</div>
+        <a href="see-more.php" class="card">
+            <img src="../../assets/img/lovemati" alt="See More">
+            <div class="card-content">
+                <i class="fas fa-search-location"></i>
+                <p>SEE MORE</p>
+            </div>
+        </a>
+    </div>
 
 
 </body>
-    <?php include("../../includes/footer.php"); ?>
+<?php include("../../includes/footer.php"); ?>
 
 </html>
